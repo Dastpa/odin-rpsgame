cbf7e9f8924d3ce495ebfe3a84820d33c59d934d
Dastpa <patrickdastey@gmail.com>
2022-07-01 15:24:01 +0100

    Completed RPS Game and UI

--- javascript.js
+++ javascript.js
@@ -2,6 +2,11 @@ let compScore = 0;
 let playerScore = 0;
 let i = 0
 
+const buttons = document.querySelectorAll('.game-button');
+const txtScore = document.querySelector(".score");
+const txtPChoice = document.querySelector(".pchoice");
+const txtCChoice = document.querySelector(".cchoice");
+
 function computerPlay() {
     let actionValue = Math.floor(Math.random() * 3);
     switch (actionValue){
@@ -32,40 +37,52 @@ function playRound(plPick, cpPick) {
 }
 
 function game(pSelect) {
+    txtCChoice.style.color =  '#c9ffe2';
+    txtPChoice.style.color =  '#c9ffe2';
+
     const PLAYER_SELECT = pSelect;
     const COMP_SELECT = computerPlay();
 
-    console.log(`Computer Selects: ${COMP_SELECT}`)
-    console.log(`Player Selects: ${PLAYER_SELECT}`)
+    txtPChoice.textContent = `Player: ${PLAYER_SELECT.toUpperCase()}`;
+    txtCChoice.textContent = `Computer: ${COMP_SELECT.toUpperCase()}`;
 
     const OUTCOME = playRound(PLAYER_SELECT, COMP_SELECT);
 
     if (OUTCOME === 0) {
-        console.log("Player Wins!");
         i++;
         playerScore++;
-    } else if (OUTCOME === 1) {
-        console.log("Tie!");
-    } else {
-        console.log("Computer Wins!");
+        txtPChoice.style.color = '#7dd181';
+    } else if (OUTCOME === 2) {
         i++;
         compScore++;
+        txtCChoice.style.color = '#7dd181';
     }
 }
 
-const buttons = document.querySelectorAll('.game-button');
+function gameReset() {
+    compScore = 0;
+    playerScore = 0;
+}
 
 buttons.forEach((btn) => {
     btn.addEventListener('click', function (e) {
+        btn.classList.add("pressed");
         game(e.target.id);
+        if (playerScore === 5) {
+            txtScore.textContent = "Player Wins Overall!"
+            gameReset();
+        } else if (compScore === 5) {
+            txtScore.textContent = "Computer Wins Overall!"
+            gameReset();
+        } else {
+            txtScore.textContent = `${playerScore} - ${compScore}`;
+        }
     })
 })
 
-const txtScore = document.querySelector(".score")
-
-
-for (i ; i < 0; i) {
-    game();
+function removeTransition(e) {
+    e.target.classList.remove('pressed');
 }
 
-playerScore > compScore ? console.log("Player Wins Overall!") : console.log("Computer Wins Overall!");
+buttons.forEach(btn => btn.addEventListener('transitionend', removeTransition));
+
